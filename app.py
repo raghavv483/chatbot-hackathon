f2e709ae589fa3bf8e0150a26b944d54134296d8
Raghav khandelwal <raghavkhandelwal483@gmail.com>
2025-07-01 00:31:26 +0530

    debug

--- app.py
+++ app.py
@@ -774,4 +774,5 @@ if __name__ == "__main__":
     
     # Run the API
     port = int(os.environ.get("PORT", 8000))
+    print(f"🚀 Starting server on port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port) 
